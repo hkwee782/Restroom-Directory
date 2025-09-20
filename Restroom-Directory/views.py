e652855ccb6f52e425bf4c602c875b1ce1558ef3
Hannah Kwee <mimi-hannah@usa.net>
2025-09-20 15:23:53 -0400

    added response.html

--- Restroom-Directory/views.py
+++ Restroom-Directory/views.py
@@ -4,4 +4,8 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return render_template("index.html", var="Hello World")
+    return render_template("index.html")
+
+@views.route("/response")
+def response():
+    return render_template("response.html")
