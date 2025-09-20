2032349f0e201af6ce00b1e7807cd5e3a9d00660
Hannah Kwee <mimi-hannah@usa.net>
2025-09-20 13:51:31 -0400

    created views.py

--- Restroom-Directory/app.py
+++ Restroom-Directory/app.py
@@ -3,9 +3,7 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.register_blueprint("views", url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug = True)
