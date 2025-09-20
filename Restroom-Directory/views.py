d50a412802346547d8dd4a8dd2b3a1b1383aec57
Hannah Kwee <mimi-hannah@usa.net>
2025-09-20 14:13:38 -0400

    updated app.py, views.py, index.html

--- Restroom-Directory/views.py
+++ Restroom-Directory/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-views = Blueprint(__name__, "views")
+views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", var="Hello World")
