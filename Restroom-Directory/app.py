08f3f72e4a404a0f7a0ad604ccf708d61c92e642
Hannah Kwee <mimi-hannah@usa.net>
2025-09-20 20:10:11 -0400

    Revert "fixed database app.py"
    This reverts commit 929b205cca2e63c4b91e7d236cce3e2788e43829.

--- Restroom-Directory/app.py
+++ Restroom-Directory/app.py
@@ -15,24 +15,18 @@ app.register_blueprint(views) # url_prefix="/views"
 
 # Databases
 class Building(db.Model):
-    __tablename__ = 'building'
-    bID = db.Column(db.Integer, primary_key=True, nullable=False)
+    buildingID = db.Column(db.Integer, primary_key=True, foreign_key=True, nullable=False)
     name = db.Column(db.String(30), nullable=False)
     lat = db.Column(db.Float, nullable=False)
     lon = db.Column(db.Float, nullable=False)
-    bathrooms = db.relationship('bathroom', 'building', lazy=True)
 
 class Bathroom(db.Model):
-    __tablename__ = 'bathroom'
-    bID = db.Column(db.Integer, db.ForeignKey('building.bID'), primary_key=True, nullable=False)
+    bathroomID = db.Column(db.Integer, primary_key=True, foreign_key=True, nullable=False)
     floor = db.Column(db.Integer, primary_key= True, nullable=False)
-    brID = db.Column(db.Integer, nullable=False)
-    reviews = db.relationship('review', 'bathroom', lazy=True)
+    ReviewID = db.Column(db.Integer, foreign_key = True, nullable=False)
 
 class Review(db.Model):
-    __tablename__ = 'review'
-    brID = db.Column(db.Integer, db.ForeignKey('bathroom.brId'), nullable=False)
-    rID = db.Column(db.Integer, primary_key=True, nullable=False)
+    reviewID = db.Column(db.Integer, primary_key=True, foreign_key=True, nullable=False)
     wheelchair = db.Column(db.Boolean, nullable=True)
     menstrual = db.Column(db.Boolean, nullable=True)
     cleanliness = db.Column(db.Integer, nullable=True)
@@ -40,7 +34,7 @@ class Review(db.Model):
 
 # functions to add data
 def initialize_buildings():
-    bID = [1,2,3,4]
+    buildingID = [1,2,3,4]
     name = ["Cathedral of Learning",
                 "Wesley W. Posvar Hall",
                 "Michael L. Benedum Hall",
@@ -50,9 +44,9 @@ def initialize_buildings():
     lon = [-79.95283, -79.9538, -79.9587, -79.9549878]
 
     for i in range(4):
-        if not Building.query.filter_by(bID=bID).first():
+        if not buildingID.query.filter_by(buildingID=buildingID).first():
             db.session.add(Building(
-                bID = bID[i],
+                buildingID = buildingID[i],
                 name = name[i],
                 lat = lat[i],
                 lon = lon[i]
@@ -62,26 +56,26 @@ def initialize_buildings():
 def add_building(id, name, lat, lon):
     if not Building.query.filter_by(buildingID=id).first():
             db.session.add(Building(
-                bID = id,
+                buildingID = id,
                 name = name,
                 lat = lat,
                 lon = lon
             ))
     db.session.commit()
 
-def add_bathroom(brID, floor, rID):
-    if not Bathroom.query.filter_by(brID=brID).first():
+def add_bathroom(bID, floor, rID):
+    if not Bathroom.query.filter_by(buildingID=bID).first():
         db.session.add(Bathroom(
-            brID=brID,
+            buildingID=bID,
             floor = floor,
             reviewID = rID
         ))
     db.session.commit()
 
 def add_review(rID, wheelchair, menstrual, clean, review):
-    if not Review.query.filter_by(rID=rID).first():
+    if not Review.query.filter_by(reviewID=rID).first():
         db.session.add(Review(
-            rID=rID,
+            reviewID=rID,
             wheelchair = wheelchair,
             menstrual = menstrual,
             clean = clean,
